83e1a21611521e1184e0e2c873e40de399dbd4aa
renan <renand3z@gmail.com>
2023-08-17 07:03:29 +0200

    application, clippboard button

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use iced::widget::{button, column, row, text};
-use iced::{clipboard, Alignment, Color, Element, Padding, Sandbox, Settings};
+use iced::{
+    clipboard, executor, Alignment, Application, Color, Command, Element, Padding, Settings, Theme,
+};
 use password_generator::get_word_pair;
 
 pub fn main() -> iced::Result {
@@ -19,43 +21,49 @@ enum Message {
     CopyToClipboard,
 }
 
-impl Sandbox for Password {
+impl Application for Password {
+    type Executor = executor::Default;
     type Message = Message;
+    type Flags = ();
+    type Theme = Theme;
 
-    fn new() -> Self {
+    fn new(_flags: ()) -> (Password, Command<Self::Message>) {
         let words = get_word_pair(WORDS);
-clipboard::write::<Message>("lol it's coppying".to_string());
+        // clipboard::write::<Message>("lol it's coppying".to_string());
 
-        Self {
-            prefix: String::from(PREFIX),
-            words,
-        }
+        (
+            Self {
+                prefix: String::from(PREFIX),
+                words,
+            },
+            Command::none(),
+        )
     }
 
     fn title(&self) -> String {
         String::from("1A! Password Generator")
     }
 
-    fn update(&mut self, message: Message) {
+    fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
         match message {
             Message::Generate => {
                 let new_words = get_word_pair(WORDS);
                 self.words = new_words;
-                // let x = clipboard1;
+                return Command::none();
             }
             Message::CopyToClipboard => {
-                let clipboard1 = clipboard::write::<Message>("lol it's coppying".to_string());
+                let clipboard1 = clipboard::write::<Message>(PREFIX.to_string() + &self.words.join(""));
                 // let clipboard = clipboard::write::<Message>("lol".to_owned());
-                clipboard1.actions();
+                return clipboard1;
             }
         }
     }
 
-    fn view(&self) -> Element<Message> {
+    fn view(&self) -> Element<Self::Message> {
         let text_size = 33;
         let cyan = Color::from_rgb(0.0, 1.0, 1.0);
         let red = Color::from_rgb(1.0, 0.0, 0.0);
-        let clipboard1 = clipboard::write::<Message>("lol".to_string());
+        // let clipboard1 = clipboard::write::<Message>("lol".to_string());
 
         // let mut row = Row::new();
 
@@ -69,23 +77,26 @@ clipboard::write::<Message>("lol it's coppying".to_string());
         //   row,
         //   button("Generate").on_press(Message::Generate)
         // ]
-
         column![
             row![
-                text(&self.prefix).size(text_size),
-                text(&self.words[0]).size(text_size).style(cyan),
-                text(&self.words[1]).size(text_size).style(red),
-                // text(&self.words[2]).size(text_size).style(cyan),
-            ],
-            // text (Message::generic(Message::Generate)),
+            text(&self.prefix).size(text_size),
+            text(&self.words[0]).size(text_size).style(cyan),
+            text(&self.words[1]).size(text_size).style(red),
             button("Generate").on_press(Message::Generate),
-            button("Copy to Clipboard").on_press(Message::CopyToClipboard),
+                    button("Copy to Clipboard")
+        .on_press(Message::CopyToClipboard)
+        ],
         ]
-        // .spacing(20)
+        // button.on_press(Message::Generate)],
+        // text (Message::generic(Message::Generate)),
+        // .button("Copy to Clipboard")
+        // .on_press(Message::CopyToClipboard)
+        // // .spacing(20)
         .padding(Padding::from(30))
         .align_items(Alignment::Center)
         // .align_self(Alignment::Center)
         .into()
+        
     }
 
     fn theme(&self) -> iced::Theme {
